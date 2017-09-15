7e5f616f3a59e9831e917043fde3d97d336a3cf6
Fred <fred@Fredui-MacBook-Air.local>
2017-09-15 22:17:25 +0900

    numgame complete

--- numgame/core.py
+++ numgame/core.py
@@ -1,17 +1,28 @@
 import random
 
-def game(name):
+def game(name,a):
     print("hello " + name)
     print('input an integer 0<num<101')
     num = random.randrange(1,101)
     while True:
-        inum = (yield)
+        flag = 0
+        numm = (yield)
+        inum = int(numm[0])
+        count = int(numm[1])
         if inum < 0 or inum > 100:
             print("Error, please input a number between 1 and 100")
         elif inum > num:
             print("too big")
+            
         elif inum < num:
             print("too small")
+            
         else: 
             print('Correct!')
+            if count <= 7:
+                print('술먹일 사람 선!택!')
+            break
+
+           
+            
 
--- numgame/numgame.py
+++ numgame/numgame.py
@@ -1,16 +1,24 @@
 from core import game
 
 name = 'Fred'
-
-cortn = game(name)
-cortn.next()
-count = 0
+a = 'someeethinggggg'
+cortn = game(name,a)
+cortn.__next__()
+count = 1
+flag = 0
+#a = 'something'
 while True:
-    thenum = int(raw_input())
-    if cortn.send(thenum) == 'Correct!':
-        cortn.close()
-        break
-        
+    thenum = int(input())
+    cortn.send([thenum, count])
+            
     print('you did ' + str(count) + ' times')
     count +=1 
-print('game over')
+    if count == 8:
+        print('술1잔!')
+    if count == 10:
+        print('술2잔!')
+    if count == 12:
+        print('술3잔!')
+    print('\n')
+
+#print('game over')
